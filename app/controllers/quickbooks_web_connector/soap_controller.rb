278a4d6be3ebce2a9f212d3e18fcf1c8503594a9
Brad Ediger <brad.ediger@madriska.com>
2017-11-29 12:37:08 -0600

    Add debug logging to SOAP endpoint itself

--- app/controllers/quickbooks_web_connector/soap_controller.rb
+++ app/controllers/quickbooks_web_connector/soap_controller.rb
@@ -5,7 +5,9 @@ module QuickbooksWebConnector
       # QWC will perform a GET to check the certificate, so we gotta respond
       render nothing: true and return if request.get?
 
+      Rails.logger.debug "[QBWC/SOAP] <<< #{request.raw_post}"
       response = SoapWrapper.route(request)
+      Rails.logger.debug "[QBWC/SOAP] >>> #{response}"
       render xml: response, content_type: 'text/xml'
     end
 
